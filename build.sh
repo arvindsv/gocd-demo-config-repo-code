cb8904c67da3b4e993953788ff698b2bb523743f
Aravind SV <arvind.sv@gmail.com>
2016-04-27 07:58:02 -0400

    A sample build file

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Let us pretend that this is building something ... and takes 20 seconds"
+
+count=0
+while [ "$count" -lt 10 ]; do
+    count=$((count + 1))
+
+    color_code=$((31 + (count % 7)))
+    printf "\e[01;${color_code}mBuilding [$count of 20]\e[00m\n"
+done
